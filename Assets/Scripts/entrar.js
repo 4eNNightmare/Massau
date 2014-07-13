d8bee90d3baa4f566b87f882e1d077c2389a4fff
4eNNightmare <nightmare.4en@hotmail.com>
2014-07-13 16:19:41 -0300

    Update003 13/07/2014
    - Removido a pasta libray para testar bug do gitignore.
    - Adicionados comentarios no script entrar.js

--- Assets/Scripts/entrar.js
+++ Assets/Scripts/entrar.js
@@ -1,19 +1,23 @@
 ﻿#pragma strict
-	public var skin : GUISkin;
-	public var logoTexture : Texture;
-	public var bgTexture : Texture;
+
+public var skin : GUISkin;
+public var logoTexture : Texture;
+public var bgTexture : Texture;
 function OnGUI(){
-	GUI.skin = skin;
-	GUI.DrawTexture(Rect(0,0,Screen.width,Screen.height), bgTexture, ScaleMode.ScaleAndCrop, true, 0);
+	GUI.skin = skin; 
+	//Determina qual sera a GUI.skin nas linhas abaixo, no caso, para a frase do GUI.Label.
+	GUI.DrawTexture(Rect(0,0,Screen.width,Screen.height), bgTexture, ScaleMode.ScaleAndCrop, true, 0); 
+	//Desenha o background cobrindo toda a tela, caso o aspect ratio da resoluçao nao for compativel, a imagem sera cortada.
 	GUI.DrawTexture(Rect(Screen.width/2 - logoTexture.width/2,100,logoTexture.width,logoTexture.height), logoTexture, ScaleMode.ScaleAndCrop, true, 0);
+	//Desenha o Logo (Titulo do Jogo).
 	GUI.Label(Rect(Screen.width/2 -300,Screen.height - 100 -((20*Screen.height)/100),600, 100),"Aperte qualquer tecla para continuar...");
+	//Escreve a frase "Aperte qualquer tecla para continuar..." na tela.
 	GUI.skin = null;
+	//Retorna o GUI.skin para Default.
 }
 
 function Update () {
-if(Input.anyKey){
-
-Application.LoadLevel("MenuP");
-
-}
+	if(Input.anyKey){ //Checa se uma tecla foi precionada.
+		Application.LoadLevel("MenuP"); //Carrega a Cena do Menu Principal.
+	}
 }
